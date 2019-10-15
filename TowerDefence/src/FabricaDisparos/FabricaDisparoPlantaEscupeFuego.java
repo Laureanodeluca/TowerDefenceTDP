6057fe78d2dd97e4670c2d8608b59215cee5f368
GinoGiordano <ginles289@gmail.com>
2019-10-15 11:15:37 -0300

    hola de nuevo
    funca?

--- TowerDefence/src/FabricaDisparos/FabricaDisparoPlantaEscupeFuego.java
+++ TowerDefence/src/FabricaDisparos/FabricaDisparoPlantaEscupeFuego.java
@@ -1,5 +1,24 @@
 package FabricaDisparos;
 
-public class FabricaDisparoPlantaEscupeFuego {
+import Personajes.Disparo;
+import Personajes.DisparoPlantaEscupeFuego;
+import Personajes.Personaje;
+
+public class FabricaDisparoPlantaEscupeFuego extends FabricaDisparo{
+
+	public FabricaDisparoPlantaEscupeFuego(Personaje disparador) {
+		super(disparador);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void generarDisparo() {
+		// TODO Auto-generated method stub
+
+        Disparo nuevoDisparo=new DisparoPlantaEscupeFuego(disparador.getX(),disparador.getY(),disparador.getAtaque(),disparador.getVelocidadDeAtaque());
+
+        hiloPuedeAtacar=new HiloPrueba(disparador);
+        hiloPuedeAtacar.start();
+	}
 
 }
--- TowerDefence/src/Personajes/DisparoPlantaEscupeFuego.java
+++ TowerDefence/src/Personajes/DisparoPlantaEscupeFuego.java
@@ -1,5 +1,33 @@
 package Personajes;
 
-public class DisparoPlantaEscupeFuego {
+import javax.swing.ImageIcon;
+
+import Visitor.Visitor;
+
+public class DisparoPlantaEscupeFuego extends Disparo {
+
+	public DisparoPlantaEscupeFuego(int x, int y, int ataque, int velocidaddeAtaque) {
+		super(x, y, ataque, velocidaddeAtaque);
+		sprite=new ImageIcon("src/main/resources/PlantaEscupefuego/bola fuego.png");
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void atacar(Personaje p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void accept(Visitor v) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int kill() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
