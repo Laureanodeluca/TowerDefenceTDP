1031b049a9fd7ea346b344ed4ea022daa3eb002f
Melina Duran <MelinaDuran@users.noreply.github.com>
2019-11-07 16:14:08 -0300

    sa

---
+++ TowerDefence/src/Nivel/NivelTres.java
@@ -0,0 +1,16 @@
+package Nivel;
+
+import Mapa.Mapa;
+
+public class NivelTres extends Nivel {
+
+	public NivelTres(Mapa m) {
+		super(m);
+		cantEnemigos=30;
+	}
+	
+	public Nivel pasarDeNivel() {
+		return null;
+	}
+
+}
